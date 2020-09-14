c248768e47254429f20c4c2e9b1636ded018a191
deltaintegrate <deltaintegrate>
2020-09-14 16:02:38 -0500

    añadiendo gitignore

--- Based-node/app.js
+++ Based-node/app.js
@@ -1,5 +1,7 @@
 const argv = require('./config/yargs').argv;
 
+const colors = require('colors');
+
 const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
 
 let comando = argv._[0];
