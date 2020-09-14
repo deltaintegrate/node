878fb9e39c3497a312baf8d720339a5491a0a4d3
deltaintegrate <deltaintegrate>
2020-09-14 16:00:41 -0500

    uso de colors y yargs

--- Based-node/app.js
+++ Based-node/app.js
@@ -1,9 +1,33 @@
-const { crearArchivo } = require('./multiplicar/multiplicar');
+const argv = require('./config/yargs').argv;
 
-let base = 'abc';
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
 
-console.log(process);
-crearArchivo(base).then(archivo => console.log(`Archivo creado: ${archivo}`))
-    .catch(err => {
-        console.log(err);
-    })
+let comando = argv._[0];
+
+switch (comando) {
+
+    case 'listar':
+        listarTabla(argv.base, argv.limite).then(
+            console.log(`valor obtenido`)
+        ).catch(e => {
+            console.log(e)
+        })
+        break;
+    case 'crear':
+        crearArchivo(argv.base, argv.limite).then(archivo => console.log(`Archivo creado: ${archivo}`))
+            .catch(err => {
+                console.log(err);
+            })
+        break;
+
+    default:
+        console.log('comando no reconocido')
+        break;
+
+}
+
+//console.log();
+
+// let argv2 = process.argv;
+
+// console.log(argv);
--- Based-node/multiplicar/multiplicar.js
+++ Based-node/multiplicar/multiplicar.js
@@ -1,8 +1,9 @@
 //requireds
 
 const fs = require('fs');
+const colors = require('colors');
 
-let crearArchivo = (base) => {
+let crearArchivo = (base, limite) => {
 
     return new Promise((resolve, reject) => {
 
@@ -12,7 +13,12 @@ let crearArchivo = (base) => {
             return;
         }
 
-        let tamano = 10
+        if (!Number(limite)) {
+            reject(`El valor introducido ${limite} no es un numero `);
+            return;
+        }
+
+        let tamano = limite
         let data = '';
 
         for (let i = 0; i <= tamano; i++) {
@@ -23,12 +29,38 @@ let crearArchivo = (base) => {
             if (err)
                 reject(err);
             else
-                resolve(`tabla = ${base}.txt`);
+                resolve(`tabla = ${base}.txt con limite: ${limite}`.blue);
         });
 
     })
 }
 
+
+let listarTabla = (base, limite = 5) => {
+
+    return new Promise((resolve, reject) => {
+
+        if (!Number(base)) {
+            reject('no encontre base numerica')
+            return;
+        }
+
+        if (!Number(limite)) {
+            reject('no encontre limite numerico')
+            return;
+        }
+
+        console.log('==========================='.green)
+        console.log(`tabla de multiplicar`.green);
+        console.log('==========================='.green);
+
+        for (let i = 0; i <= limite; i++) {
+            console.log(`en la base ${base} multiplicado ${i} : ${i*base} \n`);
+        }
+    })
+}
+
 module.exports = {
-    crearArchivo
+    crearArchivo,
+    listarTabla
 }
